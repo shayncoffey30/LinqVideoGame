792dd6d2db2e85b7e377cdb7bbdc548d2181adf2
Shayn Coffey <Shayncoffey30@gmail.com>
2025-04-17 20:56:58 -0400

    exercise completed

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//using C34PracticeLinqExercise;
+
+
+List<string> NintendoGames = new List<string>() { "PaperBoy,", "MarioBrothers", "Zelda" };
+
+NintendoGames.Add("Mike Tyson Punch out");
+NintendoGames.Add("Contra");
+
+var orderedGames = NintendoGames.OrderBy(name => name.Length);
+
+foreach (var game in orderedGames)
+{
+    Console.WriteLine(game);
+}
+
